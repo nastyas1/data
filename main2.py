6f659b1c99fa714243753fc6ba283d3e7940fa8c
nastyas1 <101515933+nastyas1@users.noreply.github.com>
2022-12-10 13:54:06 +0300

    Update main2.py

--- main2.py
+++ main2.py
@@ -4,7 +4,7 @@ import sys
 
 
 def load_image(name, colorkey=None):
-    fullname = os.path.join('data', name)
+    fullname = os.path.join('hero_move', name)
     if not os.path.isfile(fullname):
         print(f"Файл с изображением '{fullname}' не найден")
         sys.exit()
