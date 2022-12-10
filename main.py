b9230cd15b80bfbe00d1ae873ffdf86fd9f917aa
nastyas1 <101515933+nastyas1@users.noreply.github.com>
2022-12-10 12:56:57 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,48 @@
+import os
+import pygame
+import sys
+
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join('data', name)
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
+        sys.exit()
+    image = pygame.image.load(fullname)
+    if colorkey is not None:
+        image = image.convert()
+        if colorkey == -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey)
+    else:
+        image = image.convert_alpha()
+    return image
+
+
+def cursor():
+    size = 400, 400
+    screen = pygame.display.set_mode(size)
+    pygame.display.set_caption('Свой курсор мыши')
+    all_sprites = pygame.sprite.Group()
+    cursor_image = load_image('/home/nastya/Documents/code/repo-git/arrow.png')
+    cursor = pygame.sprite.Sprite(all_sprites)
+    cursor.image = cursor_image
+    cursor.rect = cursor.image.get_rect()
+    pygame.mouse.set_visible(False)
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == pygame.MOUSEMOTION:
+                cursor.rect.topleft = event.pos
+        screen.fill(pygame.Color(0, 0, 0))
+        if pygame.mouse.get_focused():
+            all_sprites.draw(screen)
+        pygame.display.flip()
+    pygame.quit()
+
+
+
+if __name__ == '__main__':
+    sys.exit(cursor())
